d69c52b2e637b4e6321cd556d80cbfd7aff6692f
henrikvi <hjviluksela@gmail.com>
2020-05-11 19:24:18 +0300

    separated from luennot repository

---
+++ index.js
@@ -0,0 +1,107 @@
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+
+morgan.token('post-body', (req, res) => {
+    if (req.method === 'POST' && req.body) {
+        return JSON.stringify(req.body)
+    }
+})
+
+app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post-body'))
+
+let persons = [
+      {
+        "name": "Arto Hellas",
+        "number": "040-123456",
+        "id": 1
+      },
+      {
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523",
+        "id": 2
+      },
+      {
+        "name": "Dan Abramov",
+        "number": "39-44-53235",
+        "id": 3
+      },
+      {
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122",
+        "id": 4
+      },
+      {
+        "name": "Janne",
+        "number": "435834987",
+        "id": 5
+      }
+]
+
+
+const generateId = () => {
+    return Math.floor(Math.random() * 999) + 1
+}
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello world</h1>')
+})
+
+app.get('/info', (req, res) => {
+    res.send(`Phonebook has info for ${persons.length} people. <br/><br/>${new Date()}`)
+})
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(person => person.id === id)
+
+    if (person) {
+        res.json(person)
+    } else {
+        res.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(person => person.id !== id)
+
+    res.status(204).end()
+})
+
+app.post('/api/persons', (req, res) => {
+
+    const body = req.body
+    
+    if (!body.name || !body.number) {
+        return res.status(400).json({
+            error: 'request must contain a name and a number'
+        })
+    }
+
+    if (persons.find(p => p.name === body.name)) {
+        return res.status(400).json({
+            error: 'name must be unique'
+        })
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateId()
+    }
+
+    persons = persons.concat(person)
+    res.json(person)
+})
+
+const PORT = 3001
+
+app.listen(PORT, () => {
+    console.log(`Phonebook server running on port ${PORT}`)
+})
