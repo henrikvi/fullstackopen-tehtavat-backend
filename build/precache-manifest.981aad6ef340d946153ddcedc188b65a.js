ee86e1549fdcce049c868d27f72c9fbe854d4afe
henrikvi <hjviluksela@gmail.com>
2020-05-12 21:33:00 +0300

    uibuild

---
+++ build/precache-manifest.981aad6ef340d946153ddcedc188b65a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "617a3c12771f46346d4604749467b7fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "678f8c1b8d05fb1c42a2",
+    "url": "/static/css/main.2e428876.chunk.css"
+  },
+  {
+    "revision": "2528ae1869b96745e7fa",
+    "url": "/static/js/2.b46f22f4.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.b46f22f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "678f8c1b8d05fb1c42a2",
+    "url": "/static/js/main.a555e08b.chunk.js"
+  },
+  {
+    "revision": "4db83e21967702cf8c1e",
+    "url": "/static/js/runtime-main.eb408f29.js"
+  }
+]);
